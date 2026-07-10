c44cc8c4cdfee1efb31b8b1193747323f46e70cb
Khemin Chepong <khemin.c@ku.th>
2026-07-10 09:59:42 +0700

    Week_2rr

---
+++ labs/week02-lab/example/test.py
@@ -0,0 +1,7 @@
+num1 = float(input("Enter first number: "))
+num2 = float(input("Enter second number: "))
+
+print(f"\nCalculations with {num1} and {num2}:")
+print(f"{num1} + {num2} = {num1 + num2}")
+print(f"{num1} - {num2} = {num1 - num2}")
+print(f"{num1} * {num2} = {num1 * num2}")10
